25c9ca9c3b2422d382233418ec9771c70f0eab7a
Derrick Njobuenwu <31127659+donadviser@users.noreply.github.com>
2024-04-11 14:15:00 +0100

    Create selenium_chromedriver_demo.py

---
+++ selenium_chromedriver_demo.py
@@ -0,0 +1,28 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+# Define Chrome options
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+# Add more options here if needed
+
+# Define paths
+user_home_dir = os.path.expanduser("~")
+chrome_binary_path = os.path.join(user_home_dir, "chrome-linux64", "chrome")
+chromedriver_path = os.path.join(user_home_dir, "chromedriver-linux64", "chromedriver")
+
+# Set binary location and service
+chrome_options.binary_location = chrome_binary_path
+webdriver_service = Service(chromedriver_path)
+
+# Initialize Chrome WebDriver
+with webdriver.Chrome(service=webdriver_service, options=chrome_options) as browser:
+    browser.get("https://www.scrapethissite.com/")
+
+    # Wait for the tagline to appear
+    tagline = browser.find_element(By.CSS_SELECTOR, "#hero > div > div > div > p")
+    print(f"{tagline.text}")
